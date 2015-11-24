327aafa0979622decefdcd87841b92dec86203c2
Mr. Is <isaac.ardis@gmail.com>
2015-11-24 11:34:18 -0500

    php -> bash syntax

--- self-portrait.sh
+++ self-portrait.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-$p=mt_rand(1,2); # execute 50% of the time
-if ( $p != 1) {
-	exit;
-} else {
+# execute 50% of the time
+p=$(( ( RANDOM % 2 )  + 1 ))
+q=1
 
-$s=mt_rand(1, 720); # wait between 1s and 12m
-sleep($s);
+if [ "$p" -eq "$q" ]
+then
+s=$(( ( RANDOM % 720 )  + 1 ))
+sleep $s
 imagesnap ~/Pictures/self-portraits/$(date +%y%m%d)-$(date +%H%M).png
-
-}
+fi
