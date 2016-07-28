a0581b559592d211610d78519837966ed00b31c7
Mr. Is <isaac.ardis@gmail.com>
2016-07-28 13:57:44 -0400

    :floppy_disk:

--- get-famous.sh
+++ get-famous.sh
@@ -1,6 +1,8 @@
 # Just images and gifs.
-rsync -avz --update ~/dev/isaacardis/app/images/selfies/ freya:~/isaacardis/images/selfies/
-rsync -avz --update ~/dev/isaacardis/app/images/gifs/ freya:~/isaacardis/images/gifs/
-rsync -avz --update ~/dev/isaacardis/app/scripts/services/selfielist.js freya:~/isaacardis/scripts/services/selfielist.js
+# Push to remote host for websiting and storage. 
+#rsync -avz --update ~/dev/isaacardis/app/images/selfies/ freya:~/isaacardis/images/selfies/
+#rsync -avz --update ~/dev/isaacardis/app/images/gifs/ freya:~/isaacardis/images/gifs/
+#rsync -avz --update ~/dev/isaacardis/app/scripts/services/selfielist.js freya:~/isaacardis/scripts/services/selfielist.js
 
+rsync -avz --update ~/Pictures/self-portraits/ freya:~/isaacardis.com/selfies/ 
 
--- publish.sh
+++
@@ -1,2 +0,0 @@
-. ~/dev/self-portrait/handle-selfies.sh > publish.log 2>&1
-. ~/dev/self-portrait/get-famous.sh > publish.log 2>&1
