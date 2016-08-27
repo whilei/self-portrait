d5e7aabf95837972ace2f5ea4835103a9a7fecff
jlanej <jlanej@gmail.com>
2016-08-27 10:02:47 -0500

    :floppy_disk:

--- self-less.sh
+++ self-less.sh
@@ -29,9 +29,11 @@ done < <(curl -s "http://isaacardis.com/selfies/?C=M;O=D")
 selflessMovie=$outDir/selfless.mp4
 
 #brew install ffmpeg
+#-framerate is the number of frames per second...
+#Not sure if the glob is globbing in perfect order yet...but looks like there are options to do that
 ffmpeg -framerate 4 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p $selflessMovie
 
 #now upload that sucker to youtube... check out https://github.com/tokland/youtube-upload
 #- has some menial work to upload
 
-$outDir/bin/youtube-upload --title="selfless" --client-secrets $outDir/client_secrets.json $selflessMovie
+#$outDir/bin/youtube-upload --title="selfless" --client-secrets $outDir/client_secrets.json $selflessMovie
