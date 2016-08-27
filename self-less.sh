8b32d4cdd1219bfef29c87ff653df54ffbe99218
jlanej <jlanej@gmail.com>
2016-08-26 21:18:21 -0500

    :floppy_disk:

--- self-less.sh
+++ self-less.sh
@@ -10,18 +10,18 @@ mkdir -p $outDir
 while read -r line
 do
   if  [[ $line =~ href=* ]]&&[[ $line =~ png ]]  ;
-then
-    png=$(echo "$line"| sed 's/<a href="//'|sed 's/">.*//')
+    then
+      png=$(echo "$line"| sed 's/<a href="//'|sed 's/">.*//')
 
-    #only get it if not here yet
-    if [ -f "$outDir/$png" ];
-		then
+        #only get it if not here yet
+      if [ -f "$outDir/$png" ];
+		    then
  		    echo "File $png exists."
-		else
-			#link it home
-   			curl "http://isaacardis.com/selfies/$png" >$outDir/"$png"
-		fi
-fi
+		  else
+			   #link it home
+   			 curl "http://isaacardis.com/selfies/$png" >$outDir/"$png"
+		  fi
+  fi
 done < <(curl -s "http://isaacardis.com/selfies/?C=M;O=D")
 
 #Now a movie maker
