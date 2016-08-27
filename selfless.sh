135faf783ead1620fc980243924d5da053e89644
jlanej <jlanej@gmail.com>
2016-08-26 19:44:26 -0500

    :floppy_disk:

--- selfless.sh
+++ selfless.sh
@@ -6,4 +6,20 @@ mkdir -p $outDir
 
 
 
-curl "http://isaacardis.com/selfies/?C=M;O=D" 
+#download available selfies
+while read -r line
+do
+  if  [[ $line =~ href=* ]]&&[[ $line =~ png ]]  ;
+then
+    png=$(echo "$line"| sed 's/<a href="//'|sed 's/">.*//')
+    #only get it if not here yet
+    if [ -f "$png" ];
+		then
+ 		    echo "File $png exists."
+		else
+   			curl "http://isaacardis.com/selfies/$png" >$outDir/"$png"
+		fi
+    echo "$png"
+fi
+done < <(curl -s "http://isaacardis.com/selfies/?C=M;O=D")
+
