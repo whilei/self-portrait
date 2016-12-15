846b5220ca34656ca27a9d17e90e3ab68e3f88e9
Mr. Is <isaac.ardis@gmail.com>
2016-12-15 10:33:36 -0500

    only crop unique originals

--- detect.go
+++ detect.go
@@ -4,12 +4,14 @@ package main
 // TODO: get it a cron to do it on the server
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
+	difflib "github.com/aryann/difflib"
 	opencv "github.com/lazywei/go-opencv/opencv"
 )
 
@@ -26,56 +28,103 @@ func exists(name string) bool {
 
 //Find the files in input directory wanted
 func getFiles(dirIn string, filetype string) []string {
-	fmt.Printf("Finding .png files in %s\n", dirIn)
+	fmt.Printf("Finding %s files in %s\n", filetype, dirIn)
 	files, err := filepath.Glob(dirIn + "*" + filetype)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Found %d files", len(files))
+	fmt.Printf("Found %d files\n", len(files))
 	return files
+}
+
+// remove abs path and "face_" from either/both original and face file names
+func stripFileName(name string) string {
+	return string(bytes.Replace([]byte(filepath.Base(name)), []byte("face_"), []byte(""), 1)) // remove 'face_' prefix
+}
+
+func getDifferentFiles(listA, listB []string) (diff []difflib.DiffRecord) {
+	return difflib.Diff(listA, listB)
+}
+
+func getUniqueOriginals(dirIn, dirOut, filetype string) []string {
+
+	// get file list from originals
+	originals := getFiles(dirIn, ".png")
+	faces := getFiles(dirOut, ".png")
+	var originalsStripped = make([]string, len(originals))
+	var facesStripped = make([]string, len(faces))
+
+	// strip file names, removing "face_" from faces
+	// and abs path from both
+	for i, faceFileName := range faces {
+		facesStripped[i] = stripFileName(faceFileName)
+	}
+	for j, originalsFileName := range originals {
+		originalsStripped[j] = stripFileName(originalsFileName)
+	}
+
+	//  Constant    Code   Meaning
+	// ----------  ------ ---------------------------------------
+	//  Common      " "    The element occurs in both sequences.
+	//  LeftOnly    "-"    The element is unique to sequence 1.
+	//  RightOnly   "+"    The element is unique to sequence 2.
+	diffs := getDifferentFiles(originalsStripped, facesStripped)
+	fmt.Printf("Diffing...\n")
+	var uniques []string
+
+	for _, diff := range diffs {
+		// fmt.Printf("%s\n", diff)
+		// should only have LeftOnly's, ie only have orignals that are not in faces'
+		if diff.Delta == difflib.LeftOnly {
+			uniqueAbsPath := dirIn + diff.Payload
+			fmt.Printf("%s\n", uniqueAbsPath)
+			uniques = append(uniques, uniqueAbsPath)
+		}
+	}
+
+	fmt.Printf("Found %d unique files.\n", len(uniques))
+
+	return uniques
 }
 
 //detects faces and crops em out
 func cropFaces(inputs []string, dirOut string, harrcascade string) {
-	err := os.MkdirAll(dirOut, 0777)
+	err := os.MkdirAll(dirOut, 0777) // makes dir if not exists
 	if err != nil {
 		fmt.Printf("Could not create directory %s\n", dirOut)
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Cropping %d images.\n", len(inputs))
+
 	for _, element := range inputs {
 
 		outPath := dirOut + "face_" + filepath.Base(element)
 
-		// check if already corresponding face image in outdir exists
-		if exists(outPath) {
-			fmt.Printf("%s already exists, not cropping.\n", outPath)
-		} else {
+		image := opencv.LoadImage(element)
+		if image == nil {
+			panic("Loading Image " + element + "failed")
+		}
+		defer image.Release()
 
-			// spin em off pew pew!
-			go func(element string) {
-				image := opencv.LoadImage(element)
-				if image == nil {
-					panic("Loading Image " + element + "failed")
-				}
-				defer image.Release()
+		//detect some faces
+		cascade := opencv.LoadHaarClassifierCascade(harrcascade)
+		faces := cascade.DetectObjects(image)
+		if len(faces) == 0 {
+			fmt.Printf("Found no face in %s\n", element)
+		}
 
-				//detect some faces
-				cascade := opencv.LoadHaarClassifierCascade(harrcascade)
-				faces := cascade.DetectObjects(image)
-				for _, value := range faces {
-					//Some invalid stuff filtering
-					if value.Width() < image.Width() && value.Height() < image.Height() && value.Width() > 0 && value.Height() > 0 {
-						//crop out the face
-						crop := opencv.Crop(image, value.X(), value.Y(), value.Width(), value.Height())
-						//save cropped
-						opencv.SaveImage(outPath, crop, 0)
-						crop.Release()
-					} else {
-						fmt.Println(value)
-					}
-				}
-			}(element)
-
+		for _, value := range faces {
+			//Some invalid stuff filtering
+			if value.Width() < image.Width() && value.Height() < image.Height() && value.Width() > 0 && value.Height() > 0 {
+				//crop out the face
+				crop := opencv.Crop(image, value.X(), value.Y(), value.Width(), value.Height())
+				//save cropped
+				opencv.SaveImage(outPath, crop, 0)
+				crop.Release()
+			} else {
+				fmt.Printf("Couldn't save: %s", value)
+			}
 		}
 	}
 }
@@ -91,13 +140,13 @@ func main() {
 	var filetype string
 	var harrcascade string
 
-	flag.StringVar(&dirIn, "dirIn", "/Users/ia/Pictures/self-portraits/", "input directory holding selfies")
-	flag.StringVar(&dirOut, "dirOut", "/Users/ia/Pictures/self-portraits-faces/", "output directory")
+	flag.StringVar(&dirIn, "dirIn", "/Users/ia/dev/self-portrait/originals_examples/", "input directory holding selfies")
+	flag.StringVar(&dirOut, "dirOut", "/Users/ia/dev/self-portrait/faces_examples/", "output directory")
 	flag.StringVar(&filetype, "filetype", ".png", "file type to detect faces, searches input directory")
 	flag.StringVar(&harrcascade, "harrcascade", "/Users/ia/gocode/src/github.com/lazywei/go-opencv/samples/haarcascade_frontalface_alt.xml", "harrcascade thing")
 
 	flag.Parse()
 
-	cropFaces(getFiles(dirIn, filetype), dirOut, harrcascade)
+	cropFaces(getUniqueOriginals(dirIn, dirOut, filetype), dirOut, harrcascade)
 
 }
---
+++ selfish.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	difflib "github.com/aryann/difflib"
+	// "os"
+	// "os/exec"
+)
+
+//Find the files in input directory wanted
+func getFiles(dirIn string, filetype string) []string {
+	fmt.Printf("Finding %s files in %s\n", filetype, dirIn)
+	files, err := filepath.Glob(dirIn + "*" + filetype)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found %d files\n", len(files))
+	return files
+}
+
+// remove abs path and "face_" from either/both original and face file names
+func stripFileName(name string) string {
+	return string(bytes.Replace([]byte(filepath.Base(name)), []byte("face_"), []byte(""), 1)) // remove 'face_' prefix
+}
+
+func getDifferentFiles(listA, listB []string) (diff []difflib.DiffRecord) {
+	return difflib.Diff(listA, listB)
+}
+
+func getUniqueOriginals(dirIn, dirOut, filetype string) []string {
+
+	// get file list from originals
+	originals := GetFiles(dirIn, ".png")
+	faces := GetFiles(dirOut, ".png")
+	var originalsStripped = make([]string, len(originals))
+	var facesStripped = make([]string, len(faces))
+
+	// strip file names, removing "face_" from faces
+	// and abs path from both
+	for i, faceFileName := range faces {
+		facesStripped[i] = stripFileName(faceFileName)
+	}
+	for j, originalsFileName := range originals {
+		originalsStripped[j] = stripFileName(originalsFileName)
+	}
+
+	//  Constant    Code   Meaning
+	// ----------  ------ ---------------------------------------
+	//  Common      " "    The element occurs in both sequences.
+	//  LeftOnly    "-"    The element is unique to sequence 1.
+	//  RightOnly   "+"    The element is unique to sequence 2.
+	diffs := getDifferentFiles(originalsStripped, facesStripped)
+	fmt.Printf("Diffing...\n")
+	var uniques []string
+
+	for _, diff := range diffs {
+		// fmt.Printf("%s\n", diff)
+		// should only have LeftOnly's, ie only have orignals that are not in faces'
+		if diff.Delta == difflib.LeftOnly {
+			uniqueAbsPath := dirIn + diff.Payload
+			fmt.Printf("%s\n", uniqueAbsPath)
+			uniques = append(uniques, uniqueAbsPath)
+		}
+	}
+
+	fmt.Printf("--> Found %d unique files.\n", len(uniques))
+
+	return uniques
+}
+
+func main() {
+	originalsDir := "/Users/ia/dev/self-portrait/originals_examples/"
+	facesDir := "/Users/ia/dev/self-portrait/faces_examples/"
+	filetype := ".png"
+	getUniqueOriginals(originalsDir, facesDir, filetype)
+}
