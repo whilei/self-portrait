748421711790e1cf665b081d4cfec3a46341642a
ia <isaac.ardis@gmail.com>
2021-03-20 10:39:10 -0500

    script to do backup

---
+++ bin/backup_SAMSUNG_T5.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+declare -a bks
+bks=(SAMSUNG_T5 SAMSUNG_T5_3)
+for bk in "${bks[@]}"
+do
+    if [[ ! -f ~/$bk/CANARY.md ]]; then
+        echo "Unmounted: $bk"
+        exit 0;
+    fi
+
+    rsync -a ~/Pictures/self-portraits/ ~/$bk/self-portraits
+    rsync -a ~/Pictures/self-portraits2/ ~/$bk/self-portraits2
+
+    echo "Backup OK: $bk $(date)"
+done
