d1dc355893c22609f93ac1cdb75fd6a07e48ae86
ia <isaac.ardis@gmail.com>
2022-11-08 09:22:33 -0800

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- bin/backup_SAMSUNG_T5.sh
+++ bin/backup_SAMSUNG_T5.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 declare -a bks
 bks=(SAMSUNG_T5 SAMSUNG_T5_3)
@@ -8,10 +9,18 @@ for bk in "${bks[@]}"
 do
     if [[ ! -f ~/$bk/CANARY.md ]]; then
         echo "Unmounted: $bk"
-        exit 0;
+        continue
+        # exit 0;
     fi
 
-    rsync -a ~/Pictures/self-portraits/ ~/$bk/self-portraits
+    # rsync -a ~/Pictures/self-portraits/ ~/$bk/self-portraits
+
+    # Development iteration of command above.
+    # https://unix.stackexchange.com/a/558240
+    # rsync -rlptgoDvz ~/Pictures/self-portraits/ ~/$bk/self-portraits
+    rsync -rgoDvz --progress ~/Pictures/self-portraits/ ~/$bk/self-portraits
+
+    # Commented because this directory is a static archive.
     # rsync -a ~/Pictures/self-portraits2/ ~/$bk/self-portraits2
 
     echo "Backup OK: $bk $(date)"
